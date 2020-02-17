5deaf55468fdb33a6b524cda1582fd762ec83ff0
Lenno Nagel <lenno@nagel.ee>
2020-02-17 14:46:11 +0200

    Remove Python 3.5

--- setup.py
+++ setup.py
@@ -27,14 +27,13 @@ test_requirements = []
 setup(
     author="Lenno Nagel",
     author_email="lenno@namespace.ee",
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
