c89126810de1c9a7e872dfbbf9e06fb151da66d0
dmitri <dmitri@namespace.ee>
2021-10-19 11:16:45 +0300

    make compatible with celery v5

--- django_datadog_logger/celery.py
+++ django_datadog_logger/celery.py
@@ -14,12 +14,20 @@ def get_celery_request():
         return None
 
 
+def get_task_name(request):
+    if isinstance(request.task, str):
+        return request.task
+    elif hasattr(request.task, "name"):
+        return request.task.name
+    return None
+
+
 def store_celery_request(func):
     @wraps(func)
     def function_wrapper(*args, **kwargs):
         try:
             if args and isinstance(args[0], Task) and hasattr(args[0], "request"):
-                local.request = args[0].request
+                local.request = {"id": args[0].request.id, "name": get_task_name(args[0].request)}
             return func(*args, **kwargs)
         finally:
             release_local(local)
