26f242c322dcbe868d6422b630d1832ad3e57129
Lenno Nagel <lenno@nagel.ee>
2022-12-19 11:44:21 +0200

    Bump version: 0.5.2 → 0.5.3

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/namespace-ee/django-datadog-logger",
-    version="0.5.2",
+    version="0.5.3",
     zip_safe=False,
 )
