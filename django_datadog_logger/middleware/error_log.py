058689576d7fabf2703f8233224ddabc5144dd66
Bruno Alla <alla.brunoo@gmail.com>
2024-01-12 09:37:37 +0000

    Skip internal Django exception for 4XX responses in ErrorLoggingMiddleware
    Fix #17

--- django_datadog_logger/middleware/error_log.py
+++ django_datadog_logger/middleware/error_log.py
@@ -1,5 +1,8 @@
 import logging
 
+from django.core.exceptions import PermissionDenied, BadRequest, SuspiciousOperation
+from django.http import Http404
+from django.http.multipartparser import MultiPartParserError
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +15,11 @@ class ErrorLoggingMiddleware:
         return self.get_response(request)
 
     def process_exception(self, request, exception):
+        if isinstance(
+            exception,
+            (PermissionDenied, Http404, MultiPartParserError, BadRequest, SuspiciousOperation),
+        ):
+            return
         logger.exception(exception)
 
     def process_response(self, request, response):
