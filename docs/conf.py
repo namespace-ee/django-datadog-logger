77ea70399f8a8157147fbe9327b804a982eaaf67
Bruno Alla <alla.brunoo@gmail.com>
2024-01-22 14:56:46 +0000

    Add rst as source suffix

--- docs/conf.py
+++ docs/conf.py
@@ -38,10 +38,7 @@ extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
 templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-source_suffix = ".md"
+source_suffix = [".rst", ".md"]
 
 # The master toctree document.
 master_doc = "index"
