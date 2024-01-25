7c75f80f3e32960fb2e505538f160da83ee69a93
Bruno Alla <alla.brunoo@gmail.com>
2024-01-25 08:57:36 +0000

    Fix includes of Markdown files in the docs

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,11 @@ import django_datadog_logger
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode"]
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
