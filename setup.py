894d3dfab6e9007a4196441791b09f18ee81c1f7
Lenno Nagel <lenno@namespace.ee>
2024-03-14 13:41:44 +0200

    Bump version: 0.7.0 → 0.7.1

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/namespace-ee/django-datadog-logger",
-    version="0.7.0",
+    version="0.7.1",
     zip_safe=False,
 )
